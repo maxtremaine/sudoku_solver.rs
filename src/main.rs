2a67d3281f724d505dc848127982325e62bb1279
maxtremaine <max.tremaine@gmail.com>
2021-11-21 13:24:21 -0500

    Added the ability to find relevant rows for a cell.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 mod pure_functions;
+mod puzzle_actions;
 
 fn main() {
-    println!("Hello, world!");
+    let _start_puzzle: [[u8; 9]; 9] = [
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ],
+        [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    ];
+
+    let test_list: Vec<u8> = (6..10).collect();
+    let remaining_digits = pure_functions::get_missing_digits(test_list);
+    for number in remaining_digits.iter() {
+        println!("{}", number)
+    }
 }
--- src/pure_functions.rs
+++ src/pure_functions.rs
@@ -10,7 +10,7 @@ mod tests {
 
     #[test]
     fn finds_missing_digits() {
-        let test_list = (4..10).collect();
+        let test_list: Vec<u8> = (4..10).collect();
         let test_output: Vec<u8> = vec![1, 2, 3];
         assert_eq!(test_output, get_missing_digits(test_list));
     }
