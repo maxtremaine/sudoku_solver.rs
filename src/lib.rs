01bc733074d40a68b064eb12466c29d842afa4d1
Max Tremaine <max.tremaine@gmail.com>
2023-05-12 15:43:47 -0400

    Added solution function for array of integers

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,23 @@ pub fn solve_file(input_sudoku_file: String, verbose: bool) -> Result<String, &'
         println!("\n");
     }
     let start_puzzle = Sudoku::from_file(input_sudoku_file)?;
-    let output = start_puzzle.get_valid_solutions(true);
+    let output = start_puzzle.get_valid_solutions(verbose);
     let output_sudoku_file = output[0].to_string();
     if verbose {
         println!("\nFinished Puzzle:\n\n{}\n", output_sudoku_file);
     }
     Ok(output_sudoku_file)
+}
+
+pub fn solve(input_values: [u8; 81], verbose: bool) -> Result<[u8; 81], &'static str> {
+    if verbose {
+        println!("\n");
+    }
+    let output_values = Sudoku::from(input_values)?
+        .get_valid_solutions(verbose)[0]
+        .numbers;
+    if verbose {
+        println!("\nFinished Puzzle:\n\n{:?}\n", output_values);
+    }
+    Ok(output_values)
 }
