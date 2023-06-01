0815edb918f849d88db25c8b588f46856ef5623d
Max Tremaine <max.tremaine@gmail.com>
2023-06-01 08:53:52 -0400

    Added documentation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,16 @@
+//! # Sudoku Solver (Rust)
+//!
+//! Solves sudoku puzzles so I don't have to, in Rust. This crate accepts input as a string or
+//! array and returns a completed sudoku puzzle. This crate can also be run with input from the
+//! /io folder.
+
 mod sudoku;
 mod blank_cell;
 mod pure_functions;
 
 use sudoku::Sudoku;
 
+/// Solves sudoku puzzles with a .sudoku file formatted string as input.
 // TODO: Add input enum and generic solve function.
 pub fn solve_file(input_sudoku_file: String, verbose: bool) -> Result<String, &'static str> {
     if verbose {
@@ -18,6 +25,12 @@ pub fn solve_file(input_sudoku_file: String, verbose: bool) -> Result<String, &'
     Ok(output_file)
 }
 
+/// Solves sudoku puzzles with an array of 81 numbers as input.
+///
+/// # Input
+///
+/// The input array starts at the top left of the sudoku puzzle progressing across and down, where
+/// the last number in the array is the bottom right. Blank cells are "0".
 pub fn solve(input_values: [u8; 81], verbose: bool) -> Result<[u8; 81], &'static str> {
     if verbose {
         println!("\n");
